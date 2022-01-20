f415583a0b2810353880f76f165efd37ffe76505
Larinha_Alanis <laraalanis6@gmail.com>
2022-01-20 07:50:26 -0300

    atividadecigarros.php
    Código da Atividade 03

---
+++ atividadecigarros.php
@@ -0,0 +1,18 @@
+<?php
+# Para iniciar o código criei as constantes para calcular os resultados da problemática
+      const ano_em_dias = 365; 
+      const minutos_de_vida = 11; 
+
+      #número de cigarros consumidos por dia
+      $cigarros = 10; 
+      #periodo determinado pelo enunciado
+      $periodo_de_tempo = 3; 
+
+      #Para saber quantos dias tem 3 anos
+      $numero_dias = $periodo_de_tempo * ano_em_dias; 
+
+      #Para saber quantos cigarros foram fumados durante os 3 anos
+      $total_de_cigarros = $cigarros * $numero_dias; 
+
+      
+
